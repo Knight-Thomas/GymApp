a18e349c33f71fa8e89bc5163582fcf2ccf1477a
Knight-Thomas <thomasmilesknight@icloud.com>
2024-05-04 22:25:12 +0100

    Database connnection made

--- MainApp.py
+++ MainApp.py
@@ -1,5 +1,13 @@
 #import modules
-import sqlite3
+import sqlite3 as sq
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import sys
 
+def DBconnect():
+    '''creates a connection to the database'''
+    try:
+        conn = sq.connect("GymAppUsers.db")
+        c = conn.cursor()
+        return conn, c
+    except sq.Error as e:
+        print(f'Database conection error: {e}')
