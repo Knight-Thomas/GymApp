47b9669a17e6ba40a8bfa6d674fbee459a75a020
Knight-Thomas <thomasmilesknight@icloud.com>
2024-05-20 22:10:28 +0100

    Saving the changes made to the GymHexWorkoutTracker.ui file

--- GymHexSwiftUI/GymHexSwiftUI/GymHexCreateAccount.swift
+++ GymHexSwiftUI/GymHexSwiftUI/GymHexCreateAccount.swift
@@ -1,18 +1,64 @@
-//
-//  GymHexCreateAccount.swift
-//  GymHexSwiftUI
-//
-//  Created by Tom Knight on 20/05/2024.
-//
-
 import SwiftUI
 
-struct GymHexCreateAccount: View {
+struct MyContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image("GymHexTopperLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 150)
+                    .padding(.top, 25) // Adjust the padding as needed
+            }
+            .frame(maxWidth: .infinity)
+            TextField("Email", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(maxWidth: .infinity)
+            TextField("Phone Number", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(maxWidth: .infinity)
+            TextField("Username", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(maxWidth: .infinity)
+            TextField("Password", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(maxWidth: .infinity)
+            Button(action: {}) {
+                Text("Create Account")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(5.0)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            Button(action: {}) {
+                Text("Already have an Account?")
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            
+            Image("GymHexMainLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 291, height: 221)
+                .padding(.bottom, 20)
+            
+            Spacer()
+        }
+        .background(Color(.sRGB, red: 51/255, green: 51/255, blue: 51/255))
+        .ignoresSafeArea()
     }
 }
 
-#Preview {
-    GymHexCreateAccount()
+struct MyContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyContentView()
+    }
 }
+
