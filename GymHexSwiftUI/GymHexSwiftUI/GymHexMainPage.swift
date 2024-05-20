c019597c08bbdb62d7f5e6b0262a49c17e44657e
Knight-Thomas <thomasmilesknight@icloud.com>
2024-05-20 22:09:31 +0100

    Adding the create account SwiftUI, removing the heght, weight and age inputs from the create account form to tidy it up and then you can add it in later

---
+++ GymHexSwiftUI/GymHexSwiftUI/GymHexMainPage.swift
@@ -0,0 +1,18 @@
+//
+//  GymHexMainPage.swift
+//  GymHexSwiftUI
+//
+//  Created by Tom Knight on 20/05/2024.
+//
+
+import SwiftUI
+
+struct GymHexMainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymHexMainPage()
+}
