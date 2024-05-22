393673814950dfed049e3085811b38ded76b260f
Knight-Thomas <thomasmilesknight@icloud.com>
2024-05-22 13:22:16 +0100

    Adding the leaderboard changes, about to implement the database to display on the leaderboard instead of the names i supply

---
+++ GymHexSwiftUI/GymHexSwiftUI/GymHexLeaderboardPage.swift
@@ -0,0 +1,18 @@
+//
+//  GymHexLeaderboardPage.swift
+//  GymHexSwiftUI
+//
+//  Created by Tom Knight on 20/05/2024.
+//
+
+import SwiftUI
+
+struct GymHexLeaderboardPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymHexLeaderboardPage()
+}
---
+++ GymHexSwiftUI/GymHexSwiftUI/GymHexSettingsPage.swift
@@ -0,0 +1,18 @@
+//
+//  GymHexSettingsPage.swift
+//  GymHexSwiftUI
+//
+//  Created by Tom Knight on 21/05/2024.
+//
+
+import SwiftUI
+
+struct GymHexSettingsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymHexSettingsPage()
+}
