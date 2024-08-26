50e95767662c8ac38026760445e71d2934bba532
Knight-Thomas <thomasmilesknight@icloud.com>
2024-08-26 20:15:46 +0100

    Finished converting what i have in python into swift

---
+++ GymHexSwiftUI/GymHexSwiftUI/GymHexCalorieTracker.swift
@@ -0,0 +1,18 @@
+//
+//  GymHexCalorieTracker.swift
+//  GymHexSwiftUI
+//
+//  Created by Tom Knight on 05/07/2024.
+//
+
+import SwiftUI
+
+struct GymHexCalorieTracker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymHexCalorieTracker()
+}
